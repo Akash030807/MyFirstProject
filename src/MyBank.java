9504d55cea65176196ce82084a327035a31b9008
AKASH <kasha030807@gmail.com>
2025-08-28 10:10:23 +0530

    Example 2 on Throw

---
+++ src/MyBank.java
@@ -0,0 +1,13 @@
+public class MyBank {
+    public static void main (String[]args){
+
+        Bank as = new Bank();
+        try{
+            as.withdraw(9999);
+            as.withdraw(1);
+        }
+        catch(InsufficientBalanceException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
