09af5b16163db8479ef1d25b71d00725925f9d4a
AKASH <kasha030807@gmail.com>
2025-09-25 10:17:07 +0530

    25/09/2025

--- src/UNIT02/MyMain.java
+++ src/UNIT02/MyMain.java
@@ -3,8 +3,16 @@ package UNIT02;
 public class MyMain {
     public static void main(String[] args) {
         Thread1 th1 = new Thread1();
-        th1.start();
         Thread2 th2 = new Thread2();
+        System.out.println(th1.getPriority());
+        th1.setPriority(10);
+        th2.setPriority(Thread.MIN_PRIORITY);
+        System.out.println(th1.getPriority());
+        System.out.println(th2.getPriority());
+        System.out.println(th1.getName());
+        th1.setName("My Thread 1");
+        System.out.println(th1.getName());
+        th1.start();
         th2.start();
     }
 }
